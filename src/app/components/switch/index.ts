0966ffe6112a2cd8c75fe879327e59425e926d4b
Raymond Cheung <iray100200@hotmail.com>
2018-05-01 12:28:04 +0800

    update

--- src/app/components/switch/index.ts
+++ src/app/components/switch/index.ts
@@ -1,22 +1,43 @@
-import { Component, ViewChild, OnInit, ElementRef, ViewContainerRef, Input } from '@angular/core';
-import * as ResJson from './res-layouts.json';
-import { MXService } from '../../../services/mx.service';
+import {
+    Component,
+    ViewChild,
+    OnInit,
+    ElementRef,
+    ViewContainerRef,
+    Input
+} from "@angular/core";
+import * as ResLayouts from "./res-layouts.json";
+import * as ResComponents from "./res-components.json";
+import { MXService } from "../../../services/mx.service";
+import { Router, RouterState, ActivatedRoute } from "@angular/router";
+import { Observable } from "rxjs/Observable";
 
 @Component({
-    selector: '[mx-app-switch]',
-    templateUrl: './template.html',
-    styleUrls: ['./style.scss']
+    selector: "[mx-app-switch]",
+    templateUrl: "./template.html",
+    styleUrls: ["./style.scss"]
 })
 export class MXSwitchComponent implements OnInit {
-    resJson: Array<any> = <any>ResJson;
+    resJson: Array<any>;
     private resize: number = 0;
-    component;
-    @ViewChild('virtualWin') virtualWin: ElementRef;
-    @ViewChild('templateHost', { read: ViewContainerRef }) templateHost;
+    location: string;
+    constructor(router: Router) {
+        const state: RouterState = router.routerState;
+        const root: ActivatedRoute = state.root;
+        const child = root.firstChild;
+        child.params.forEach(p => {
+            this.location = p.location;
+            this.resJson = this.location === "layout" ? <any>ResLayouts : <any>ResComponents;
+        });
+    }
+    @ViewChild("virtualWin") virtualWin: ElementRef;
+    @ViewChild("templateHost", { read: ViewContainerRef })
+    templateHost;
     ngOnInit() {
-        window.addEventListener('resize', () => {
+        window.addEventListener("resize", () => {
             this.resize++;
         });
+        console.log(this.location);
     }
     get vwidth(): number {
         return this.virtualWin.nativeElement.offsetWidth;
