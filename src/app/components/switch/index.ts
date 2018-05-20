65fd1b061b82fd9124f95bf6b0becc33fe76ef2b
Raymond Cheung <iray100200@hotmail.com>
2018-05-20 20:50:31 +0800

    update

--- src/app/components/switch/index.ts
+++ src/app/components/switch/index.ts
@@ -1,19 +1,23 @@
-import { Component, ViewChild, OnInit, ElementRef, ViewContainerRef, ComponentFactoryResolver } from "@angular/core";
+import { Component, ViewChild, OnInit, ElementRef, ViewContainerRef, ComponentFactoryResolver, AfterViewInit } from "@angular/core";
 import { MXComponentServiceProvider } from "common/services";
 import { MXRouterService } from 'common/services';
 import { MXSwitchToComponents } from '../switch-components';
 import { MXSwitchToContainers } from '../switch-containers';
 import { MXSwitchToLayouts } from '../switch-layouts';
+import { fromEvent } from "rxjs/observable/fromEvent";
 
 @Component({
     selector: "[mx-app-switch]",
     templateUrl: "./template.html",
     styleUrls: ["./style.scss"]
 })
-export class MXSwitchComponent implements OnInit {
+export class MXSwitchComponent implements OnInit, AfterViewInit {
     resJson: Array<any>;
     private resize: number = 0;
     location: string;
+    wheelDelta: number = 1;
+    MAX_Z: number = 1.6;
+    MIN_Z: number = 0.3;
     @ViewChild('switchHost', { read: ViewContainerRef }) switchHost: ViewContainerRef;
     componentMap: any = {
         '/switch/layout': MXSwitchToLayouts,
@@ -39,6 +43,13 @@ export class MXSwitchComponent implements OnInit {
         window.addEventListener("resize", () => {
             this.resize++;
         });
+    }
+    ngAfterViewInit() {
+        fromEvent(this.virtualWin.nativeElement, 'mousewheel').subscribe((f: WheelEvent) => {
+            let delta = (this.wheelDelta + f.wheelDeltaY / 1200);
+            this.wheelDelta = delta < this.MIN_Z ? this.MIN_Z : delta > this.MAX_Z ? this.MAX_Z : delta;
+            this.virtualWin.nativeElement.style.zoom = this.wheelDelta;
+        })
     }
     get vwidth(): number {
         return this.virtualWin.nativeElement.offsetWidth;
